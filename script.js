3fe1ea134f1bc8c3e865189be7d173a4a356e7af
MarcioJrPC <marciofsantos65@gmail.com>
2022-11-02 20:03:54 -0300

    pegando quizzes da api-I

---
+++ script.js
@@ -0,0 +1,22 @@
+
+const promisse = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
+promisse.then(adicionaQuizz);
+promisse.catch(function (){alert("erro")});
+
+
+function adicionaQuizz(resultadoAPI){
+    alert("");
+    const array = resultadoAPI.data;
+    const quizzLista = document.querySelector(".quizzes__lista");
+    quizzLista.innerHTML = "";
+
+    for(let i = 0; i < array.length; i++){
+        quizzLista.innerHTML +=`
+        <div class="quizz x${array[i].id}">
+        <!--img src="${array[i].image}" alt="gato"/-->
+        <span>${array[i].title}</span>
+        </div>
+        `
+        console.log("a")
+    }
+}
